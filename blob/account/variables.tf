fa469d71d228035924b93d80c1055c2fe06827ba
Mark Tinderholt <mark@vitalsigyn.com>
2020-05-12 22:52:30 -0400

    add blob account / container

---
+++ blob/account/variables.tf
@@ -0,0 +1,7 @@
+variable "resource_group_name" { }
+variable "location" { }
+variable "app_name" { }
+variable "env_name" { }
+variable "name" { }
+variable "tier" { default = "Standard" }
+variable "type" { default = "GRS" }
