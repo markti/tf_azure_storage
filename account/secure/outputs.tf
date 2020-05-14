da748b43023d1750653e0ea2df96d6030bc125bd
Mark Tinderholt <mark@vitalsigyn.com>
2020-05-14 16:25:19 -0400

    fix outputs for secured version

--- account/secure/outputs.tf
+++ account/secure/outputs.tf
@@ -1,13 +1,13 @@
 output "id" {
-  value = azurerm_storage_account.storage_account.id
+  value = module.storage_account.id
 }
 output "name" {
-  value = azurerm_storage_account.storage_account.name
+  value = module.storage_account.name
 }
 
 output "primary_endpoint" {
-  value = azurerm_storage_account.storage_account.primary_blob_endpoint
+  value = module.storage_account.primary_endpoint
 }
 output "primary_connection_string" {
-  value = azurerm_storage_account.storage_account.primary_connection_string
+  value = module.storage_account.primary_connection_string
 }
