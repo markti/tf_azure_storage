2a1a7da9027110128d0167e3e0f45c2a00be8e4e
Mark Tinderholt <mark@vitalsigyn.com>
2020-12-12 09:51:27 -0500

    add security settings

--- account/base/variables.tf
+++ account/base/variables.tf
@@ -5,3 +5,15 @@ variable "env_name" { }
 variable "name" { }
 variable "tier" { default = "Standard" }
 variable "type" { default = "GRS" }
+
+variable "security_settings" {
+  type = object({
+    allow_blob_public_access    = bool
+    min_tls_version             = string
+  })
+  default = {
+    allow_blob_public_access            = false
+    min_tls_version                     = "TLS1_2"
+  }
+
+}
